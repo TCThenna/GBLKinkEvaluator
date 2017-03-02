a552029d4d0145f9265beb821ca15d7250bfb88c
TCThenna <hendrik.jansen@desy.de>
2017-03-02 17:00:46 +0100

    Add cmake files

---
+++ cmake/FindGBL.cmake
@@ -0,0 +1,44 @@
+# - Find GBL installation
+# This module tries to find the libGBL installation on your system.
+# Once done this will define
+#
+#  GBL_FOUND - system has GBL installed
+#  GBL_INCLUDE_DIR - ~ the GBL include directory 
+#  GBL_LIBRARY - Link these to use GBL
+
+MESSAGE(STATUS "Looking for GBL...")
+
+FIND_PATH(GBL_INCLUDE_DIR GblPoint.h
+  HINTS "${PROJECT_SOURCE_DIR}/../../../../../../GBL/V01-16-03/include"
+)
+
+FIND_PATH(GBL_LIBRARY_DIR libGBL.so
+  HINTS "${PROJECT_SOURCE_DIR}/../../../../../../GBL/V01-16-03/lib"
+)
+
+FIND_LIBRARY(GBL_LIBRARY NAMES libGBL.so
+  HINTS "${PROJECT_SOURCE_DIR}/../../../../../../GBL/V01-16-03/lib"
+)
+
+set(GBL_LIBRARIES ${GBL_LIBRARY})
+
+IF (GBL_LIBRARY)
+  IF(GBL_INCLUDE_DIR)
+    set(GBL_FOUND TRUE)
+    #set(GBL_LIBRARIES "${GBL_LIBRARIES}/libGbl.so")
+    MESSAGE(STATUS "Found libGBL: ${GBL_INCLUDE_DIR}, ${GBL_LIBRARIES}")
+  ELSE(GBL_INCLUDE_DIR)
+    set(GBL_FOUND FALSE)
+    MESSAGE(STATUS "GBL headers NOT FOUND. Make sure to install the headers.")
+  ENDIF(GBL_INCLUDE_DIR)
+ELSE (GBL_LIBRARY)
+    set(GBL_FOUND FALSE)
+    MESSAGE(FATAL_ERROR "libGBL NOT FOUND.")
+ENDIF (GBL_LIBRARY)
+
+set(GBL_INCLUDE_DIR
+    ${GBL_INCLUDE_DIR}
+)
+
+set(LD_LIBRARY_PATH ${LD_LIBRARY_PATH} ${GBL_LIBRARY_DIR})
+
---
+++ cmake/config.cmake.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// CMake uses config.cmake.h to generate config.h within the build folder.
+#ifndef GBLKINKEVALUATOR_CONFIG_H
+#define GBLKINKEVALUATOR_CONFIG_H
+
+#define PACKAGE_NAME "@CMAKE_PROJECT_NAME@"
+#define PACKAGE_VERSION "@GBLKINKEVALUATOR_LIB_VERSION@"
+#define PACKAGE_STRING PACKAGE_NAME " " PACKAGE_VERSION
+
+#define PACKAGE_SOURCE_DIR "@PROJECT_SOURCE_DIR@"
+
+#endif
